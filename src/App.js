83cb14ba995b9d1549e9908690b42523aa81e6e0
romantrynko <trynkoroman25@gmail.com>
2020-11-20 18:48:54 +0200

    hooks_done

--- src/App.js
+++ src/App.js
@@ -1,39 +1,55 @@
-import React, { useEffect, useReducer } from 'react';
+import React, { useEffect, useReducer, useState, memo, useCallback, useMemo } from 'react';
 
-const initialState = {
-  count: 0,
-  name: 'test',
-  token: ''
-}
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case 'increment':
+//       return { ...state, count: state.count + 1, name: state.name + 't' };
+//     case 'decrement':
+//       return { count: state.count - 1 };
+//     default:
+//       return state;
+//   }
+// }
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'increment':
-      return { ...state, count: state.count + 1, name: state.name + 't' };
-    case 'decrement':
-      return { count: state.count - 1 };
-    default:
-      return state;
-  }
-}
+const Test = memo(({ name, onClickHandler }) => {
+  console.log('rerender');
+  return <h2 onClick={onClickHandler}>Random component</h2>
+})
+
 
 function App() {
 
-  // const [counter, setCounter] = useState(0);
-  // const [counter1, setCounter1] = useState(0);
+  const [counter, setCounter] = useState(0);
+  const [counter1, setCounter1] = useState(10);
 
-  const [state, dispatch] = useReducer(reducer, initialState);
-  console.log({state});
+  const fn = useCallback(() => {
+    setCounter1(counter1 + 1);
+  }, [counter1]);
 
-  // useEffect(() => {}, [counter])
+  const calcSomeHardStuff = () => {
+    let val = 0;
+    for(let i = 0; i < counter1; i++) {
+      console.log(i);
+      val++
+    }
+
+    return val;
+  }
+
+  const someValue = useMemo(()=> calcSomeHardStuff(), [counter1]);
 
   return (
     <div className='App'>
       <h1 onClick={() => {
-        dispatch({type: 'increment', payload: [1,2,3]});
-      }}>Hello {state.count} - {state.name}</h1>
+        setCounter(counter + 1)
+      }}
+      >
+        Hello {counter} {someValue}
+      </h1>
+      <Test name='test' onClickHandler={fn} />
     </div>
   );
 }
 
 export default App;
+ 
