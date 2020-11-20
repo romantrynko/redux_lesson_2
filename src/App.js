2c951f2f6eaeb94f244daa54f18ab4d7e7bd38d0
romantrynko <trynkoroman25@gmail.com>
2020-11-20 17:58:29 +0200

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer } from 'react';
+
+const initialState = {
+  count: 0,
+  name: 'test',
+  token: ''
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return { ...state, count: state.count + 1, name: state.name + 't' };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+}
 
 function App() {
+
+  // const [counter, setCounter] = useState(0);
+  // const [counter1, setCounter1] = useState(0);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log({state});
+
+  // useEffect(() => {}, [counter])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 onClick={() => {
+        dispatch({type: 'increment', payload: [1,2,3]});
+      }}>Hello {state.count} - {state.name}</h1>
     </div>
   );
 }
